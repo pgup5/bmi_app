1564d2e41809f16b99570a45afe21f8bac906130
Capybara <11739427+pgup5@users.noreply.github.com>
2024-07-25 12:54:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+
+st.title("BMI Calculator")
+
+# Gender selection
+gender = st.radio("Gender", ("Male", "Female"))
+
+# Height input
+height = st.slider("Height (cm)", 150, 190)
+
+# Weight input
+weight = st.slider("Weight (kg)", 40, 200)
+
+# Calculate BMI
+bmi = weight / ((height / 100) ** 2)
+
+# Display Result
+st.write("## Result")
+st.write(f"**BMI:** {bmi:.1f}")
+
+# Interpretation
+st.write("## Interpretation")
+if bmi < 18.5:
+    st.write("Underweight")
+elif 18.5 <= bmi < 24.9:
+    st.write("Normal weight")
+elif 25 <= bmi < 29.9:
+    st.write("Overweight")
+else:
+    st.write("Obesity")
+
